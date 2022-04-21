767c4e5c049040947f2b63bcfd03cbe6d8874233
Gustavo Iglesias <gustavo.iglesias40@gmail.com>
2022-04-20 23:11:52 -0300

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader,  IonIcon,  IonItem, IonLabel, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+//import ExploreContainer from '../components/ExploreContainer';
+import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+    
+      
+      <IonContent>
+    <IonGrid>
+      <IonRow>
+        <IonCol size='4'>ion-col</IonCol>
+        <IonCol size='8'>ion-col</IonCol>
+       
+      </IonRow>
+
+      
+    </IonGrid>
+  </IonContent>
+   
+    
   );
 };
 
